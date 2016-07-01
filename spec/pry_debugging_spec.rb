1de283f5f95645ee8b8f2fdfcbc2bd212dc0b0b9
Lani1321 <lani.berry13@gmail.com>
2016-07-01 13:33:36 -0400

    fixed method to have a return value of 5 by using pry

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -1,9 +1,15 @@
 require 'pry'
 require './lib/pry_debugging.rb'
 
-describe "#plus_two" do 
+describe "#plus_two" do
 	it "takes in a number as an argument and returns the sum of that number and 2" do
 		expect(plus_two(3)).to eq(5)
 	end
 
+end
+
+def plus_tow(num)
+  num + 2
+  num
+  binding.pry
 end
