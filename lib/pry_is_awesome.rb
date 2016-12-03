4edc0465084339ba9252fd2a35e7db5929cb0f16
Angela Bardysh <angelabardysh@gmail.com>
2016-12-03 03:46:37 +0000

    Done.

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -5,7 +5,7 @@ def prying_into_the_method
 	puts inside_the_method
 	puts "We're about to stop because of pry!"
 	binding.pry
-	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
+	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!"
 	puts this_variable_hasnt_been_interpreted_yet
 end
 
