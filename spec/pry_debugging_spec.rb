a253dd8c3a400108ed66b6edb91b203359bbfb83
Monica  Karasek <mlkarasek@gmail.com>
2017-10-17 05:03:13 +0000

    Done.

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -1,7 +1,14 @@
+def plus_two(num)
+    num + 2
+    num
+    binding.pry
+end
+
+
 require 'pry'
 require './lib/pry_debugging.rb'
 
-describe "#plus_two" do 
+describe "#plus_two" do
 	it "takes in a number as an argument and returns the sum of that number and 2" do
 		expect(plus_two(3)).to eq(5)
 	end
